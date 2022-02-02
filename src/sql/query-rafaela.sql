b83dd86430d5596c1edbe251398d9bdea73c2073
Rafaela Dias <93568221+rafaeladns@users.noreply.github.com>
2022-02-02 18:29:37 -0300

    Query top 10 jogos gratuitos mais baixados

--- src/sql/query-rafaela.sql
+++ src/sql/query-rafaela.sql
@@ -11,3 +11,10 @@ FROM steam
 WHERE price != 0
 ORDER BY owners
 LIMIT 10;
+
+-- Top 10 jogos gratuitos mais baixados--
+SELECT name, owners, price
+FROM steam
+WHERE price = 0
+ORDER BY owners DESC
+LIMIT 10;
