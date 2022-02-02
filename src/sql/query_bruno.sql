453dc268827b162f626fea01f2e59117dec6ecd9
brunosa91 <brunosantana.engenharia@gmail.com>
2022-02-02 12:31:22 -0300

    inserida a quarta query que relaciona os 10 setup para mac dos 10 jogos mais jogados

--- src/sql/query_bruno.sql
+++ src/sql/query_bruno.sql
@@ -25,3 +25,12 @@ ON srd.steam_appid = s.appid
 ORDER BY s.owners DESC
 LIMIT 10;
 
+--Quais os 10 setups para mac dos jogos mais jogados?
+
+SELECT s.owners, s.name, s.release_date, srd.mac_requirements
+FROM steam AS s
+INNER JOIN steam_requirements_data as srd
+ON srd.steam_appid = s.appid
+ORDER BY s.owners DESC
+LIMIT 10;
+
