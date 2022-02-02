05739c7ec674626a2f9b1602985c59ba92a4a268
brunosa91 <brunosantana.engenharia@gmail.com>
2022-02-02 12:30:10 -0300

    inseria da terceira query que relaciona os setups para linux dos 10 jogos mais jodados

--- src/sql/query_bruno.sql
+++ src/sql/query_bruno.sql
@@ -16,3 +16,12 @@ ON srd.steam_appid = s.appid
 ORDER BY s.owners DESC
 LIMIT 10;
 
+--Quais os 10 setups para linux dos jogos mais jogados?
+
+SELECT s.owners, s.name, s.release_date, srd.linux_requirements
+FROM steam AS s
+INNER JOIN steam_requirements_data as srd
+ON srd.steam_appid = s.appid
+ORDER BY s.owners DESC
+LIMIT 10;
+
