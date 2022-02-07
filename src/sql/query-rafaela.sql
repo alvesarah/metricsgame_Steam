017e1fff590a7b25ebf73fa6fe7bff911b53ae4e
Rafaela Dias <93568221+rafaeladns@users.noreply.github.com>
2022-02-06 22:36:29 -0300

    Querys com perguntas atualizadas

--- src/sql/query-rafaela.sql
+++ src/sql/query-rafaela.sql
@@ -1,20 +1,26 @@
--- Top 10 jogos mais vendidos-- 
-SELECT name, owners, price
+-- Jogos entre 1998 e 2005 com avaliações mais positivas --
+SELECT release_date,name,appid,positive_ratings FROM steam 
+WHERE release_date BETWEEN '1998%' AND '2005%' ORDER BY positive_ratings DESC LIMIT 5;
+
+-- Porcentagem de jogos single-player em relação a todos os jogos--
+SELECT ROUND(((SELECT count(categories) FROM steam WHERE categories LIKE "Single-player")/count(categories) * 100),2)
+AS porcentagem_categories FROM steam;
+
+-- 10 jogos de ação recomendados para 18 anos mais jogados --
+SELECT name, owners, required_age, genres
 FROM steam
-WHERE price != 0
+WHERE required_age >= 18 and genres = 'Action'
 ORDER BY owners DESC
 LIMIT 10;
 
--- Top 10 jogos menos vendiddos--
-SELECT name, owners
-FROM steam
-WHERE price != 0
-ORDER BY owners
-LIMIT 10;
+-- Porcentagem de jogos de ação em relação a todos os jogos--
+SELECT Round(((SELECT count(genres) AS action_genre FROM steam WHERE genres LIKE "action")/count(genres) * 100),2) 
+AS action_percentage FROM steam;
 
--- Top 10 jogos gratuitos mais baixados--
-SELECT name, owners, price
+-- Desenvolvedores com maior número de avaliações negativas dos jogos Multi-Player de 2014--
+SELECT developer, name, categories, negative_ratings, release_date
 FROM steam
-WHERE price = 0
-ORDER BY owners DESC
-LIMIT 10;
+WHERE categories NOT LIKE '%single-player%'
+AND categories LIKE '%Multi-Player%'
+AND release_date LIKE '2014%'
+ORDER BY negative_ratings DESC;
