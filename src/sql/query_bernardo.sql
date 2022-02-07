0c076d8311d607a8e5cbb31f105ab2f172e0bcbf
bernardobdr <bernardosilvabdr@gmail.com>
2022-02-07 00:01:41 -0300

    commit com as últimas alterações

--- src/sql/query_bernardo.sql
+++ src/sql/query_bernardo.sql
@@ -1,25 +1,45 @@
 CREATE DATABASE steam;
 USE steam;
-SELECT * FROM steam;
+SELECT name FROM steam;
+
+ALTER TABLE steam ADD PRIMARY KEY (appid);
 
 -- Top 5 jogos mais populares (Com base no tempo gasto jogando e segundo as avaliações positivas)
-SELECT name AS nome_do_jogo, positive_ratings AS avaliações_positivas, average_playtime AS tempo_gasto_jogando FROM steam WHERE positive_ratings > 9000 AND average_playtime > 10000;
+SELECT 
+    name AS nome_do_jogo,
+    positive_ratings AS avaliações_positivas,
+    average_playtime AS horas_gastas_jogando
+FROM
+    steam
+WHERE
+    positive_ratings > 9000
+        AND average_playtime > 10000
+LIMIT 5;
 
--- Quantidade de jogos oferecidos gratuitamente
-SELECT name AS nome_do_jogo, price AS preco_do_jogo FROM steam WHERE price = 0.00;
-
--- Quantidade de jogos comercializados
-SELECT name AS nome_do_jogo FROM steam;
-
--- Jogo mais caro comercializado
-SELECT max(price) FROM steam;
+-- Jogos que precisam ter idade mínima de 18 anos, do gênero ação e que rodam apenas em windows
+SELECT 
+    name AS nome_do_jogo,
+    required_age AS idade_obrigatória,
+    genres AS genero,
+    platforms AS sistema_operacional
+FROM
+    steam
+WHERE
+    required_age = 18
+        AND genres LIKE 'Action'
+        AND platforms = 'windows'
+ORDER BY name ASC LIMIT 15;
 
 -- Quantidade em percentual de jogos disponibilizados gratuitamente em relação ao total de jogos
-SELECT ROUND(((SELECT count(price) FROM steam WHERE price like 0) / count(price) * 100), 2) 
-AS porcentagem_jogos_gratuitos FROM steam;
-
--- Jogos que o usuário precisa ter entre 16 e 18 anos para jogar
-SELECT name AS nome_do_jogo, required_age AS idade_para_jogar FROM steam WHERE required_age BETWEEN 16 AND 18;
-
+SELECT 
+    ROUND(((SELECT 
+                    COUNT(price)
+                FROM
+                    steam
+                WHERE
+                    price = 0) / COUNT(price) * 100),
+            2) AS porcentagem_jogos_gratuitos
+FROM
+    steam; 
 
 
