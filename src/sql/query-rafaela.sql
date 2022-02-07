d15bf000383363dd16fe84e120854c0933047254
Rafaela Dias <93568221+rafaeladns@users.noreply.github.com>
2022-02-06 23:13:35 -0300

    atualização das queries

--- src/sql/query-rafaela.sql
+++ src/sql/query-rafaela.sql
@@ -1,17 +1,6 @@
--- Jogos entre 1998 e 2005 com avaliações mais positivas --
-SELECT release_date,name,appid,positive_ratings FROM steam 
-WHERE release_date BETWEEN '1998%' AND '2005%' ORDER BY positive_ratings DESC LIMIT 5;
-
 -- Porcentagem de jogos single-player em relação a todos os jogos--
 SELECT ROUND(((SELECT count(categories) FROM steam WHERE categories LIKE "Single-player")/count(categories) * 100),2)
 AS porcentagem_categories FROM steam;
-
--- 10 jogos de ação recomendados para 18 anos mais jogados --
-SELECT name, owners, required_age, genres
-FROM steam
-WHERE required_age >= 18 and genres = 'Action'
-ORDER BY owners DESC
-LIMIT 10;
 
 -- Porcentagem de jogos de ação em relação a todos os jogos--
 SELECT Round(((SELECT count(genres) AS action_genre FROM steam WHERE genres LIKE "action")/count(genres) * 100),2) 
