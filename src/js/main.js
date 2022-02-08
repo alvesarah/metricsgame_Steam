0433360f4c64537a78f5afec6b74b3de28d1e36d
Mauriciiow <mauricioliveira639@gmail.com>
2022-02-08 02:04:54 -0300

    adicionando funcao do botao reset

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,41 @@ function typeWritter(elemento){
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
         setTimeout(() => elemento.innerHTML += letra, 60 * i);
-        console.log(letra);
+       
     });
 }
 
 const titulo = document.querySelector('p');
 
 typeWritter(titulo);
+
+let arrPerguntas = []
+function adiciona() {
+    function adiciona(pergunta) {arrPerguntas.push(pergunta)}
+    $('#perguntaUm').click(()=>{
+        adiciona('perguntaUm' )
+        $('#perguntaUm').css('display', 'none')
+    }) 
+    $('#perguntaDois').click(()=>{
+        adiciona('perguntaDois')
+        $('#perguntaDois').css('display', 'none')
+    }) 
+    $('#perguntaTres').click(()=>{
+        adiciona('perguntaTres')
+        $('#perguntaTres').css('display', 'none')
+    }) 
+    $('#perguntaQuatro').click(()=>{
+        adiciona('perguntaQuatro')
+        $('#perguntaQuatro').css('display', 'none')
+    }) 
+    
+    let myInterval = setInterval(()=>{
+        if (arrPerguntas.length > 0) {
+            if (arrPerguntas.length >= 3 ) {
+               
+                clearInterval(myInterval)
+            }       
+        }
+       }, 1000)
+}
+adiciona();
