7aa8476e275ab41bab025ce260686048caff60e5
sashamoon <sarah.alves8@hotmail.com>
2022-02-09 07:14:07 -0300

    Correção de erros e inserção de músicas nas fases que faltavam

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ function typeWritter(elemento){
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 60 * i);
+        setTimeout(() => elemento.innerHTML += letra, 50 * i);
        
     });
 }
